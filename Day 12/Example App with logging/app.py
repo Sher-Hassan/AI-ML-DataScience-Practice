de9317442a168fb79ca667765a4ae01b59660d67
Sher Hassan <sherhassan739@gmail.com>
2026-03-18 10:17:54 +0500

    practicing logging using example done

--- Day 12/Example App with logging/app.py
+++ Day 12/Example App with logging/app.py
@@ -11,3 +11,36 @@ logging.basicConfig(
         logging.StreamHandler()  # Outputs log messages to console (stdout)
     ]
 )
+
+## Creating modules/loggers
+
+logger = logging.getLogger("ArithemeticApp")
+
+def add(a, b):
+    result = a + b
+    logger.debug(f"Adding {a} + {b} = {result}")
+    return result
+
+def subtract(a, b):
+    result = a - b
+    logger.debug(f"Subtracting {a} - {b} = {result}")
+    return result
+
+def multiply(a, b):
+    result = a * b
+    logger.debug(f"Multiplying {a} * {b} = {result}")
+    return result
+
+def divide(a, b):
+    try:
+        result = a / b
+        logger.debug(f"Dividing {a} / {b} = {result}")
+        return result
+    except ZeroDivisionError:
+        logger.error("Division by zero error")
+        return None
+    
+add(10,15)
+subtract(25,3)
+multiply(10,3)
+divide(80,4)
